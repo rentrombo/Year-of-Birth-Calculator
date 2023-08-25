29f2c2f32860dcb4d7c8fe0b4e5d81fc926806d3
Rowen Trombo <rowentrombo@gmail.com>
2023-08-24 20:23:15 -0500

    comments pseudo

---
+++ main.py
@@ -0,0 +1,17 @@
+#Birth year calculator
+
+#output ask for name
+print('What is your name?')
+#accept input and store in string var
+name = input()
+#output ask for age
+print('What is your age?')
+#accept input and store in int var
+
+#calc today's year
+
+#convert year to int
+
+#subtract today's year by age
+
+#print name and age
